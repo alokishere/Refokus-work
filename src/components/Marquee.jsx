4107e9e88695da8542590b8f50fb48ea8fb17e42
alokishere <alokvishwakarmabdh@gmail.com>
2025-08-30 23:30:48 +0530

    style issue

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import '../Marquee.css' // import custom CSS for animations
+
+import './marquee.css' // import custom CSS for animations
 
 const Marquee = () => {
   const images = [
